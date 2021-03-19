b8b46201aeb8cb3c15c5fb6d93249988008ec00a
Moreno Andrade <laforetmoreno@gmail.com>
2021-03-19 11:27:16 -0300

    Add first feed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 
 import Stories from 'react-insta-stories';
+import Rating from 'react-rating';
+
 import closeIcon from './icons/close.svg';
 import starIcon from './icons/star.svg';
-import Rating from 'react-rating';
+import like from './icons/like.svg';
+import feed from './icons/feed.svg';
 
 import './App.scss';
 
@@ -40,6 +43,7 @@ const Content = ({
         />
       </div>
       <button
+        className="goto"
         onClick={() =>
           (window.location.href =
             'https://www.ifood.com.br/delivery/teresopolis-rj/number-one-chicken--varzea/874d756e-5a8b-4a1e-8a63-97cfa96099d7?prato=532516686')
@@ -115,37 +119,85 @@ const App = () => {
       },
     ];
 
-    return array.map(x => {
-      return {
-        content: props => {
-          return (
-            <Content
-              onClose={() => setShowStories(false)}
-              id={x.id}
-              name={x.name}
-              avatar={x.avatar}
-              createdAt={x.createdAt}
-              image={x.image}
-              description={x.description}
-            />
-          );
-        },
-        duration: x.duration,
-      };
-    });
+    return {
+      stories: array.map(x => {
+        return {
+          content: props => {
+            return (
+              <Content
+                onClose={() => setShowStories(false)}
+                id={x.id}
+                name={x.name}
+                avatar={x.avatar}
+                createdAt={x.createdAt}
+                image={x.image}
+                description={x.description}
+              />
+            );
+          },
+          duration: x.duration,
+        };
+      }),
+      feed: array,
+    };
   };
 
-  // setTimeout(() => {
-  //   setShowStories(false);
-  // }, 22000);
-
   return (
-    <>
-      {!showStories && (
-        <button onClick={() => setShowStories(true)}>Mostrar stories</button>
-      )}
-      {showStories && <StoriesProvider stories={makeStories()} />}
-    </>
+    <div className="wrapper">
+      <div className="feed-header">
+        <h1 className="title">Veja os pratos mais amados pelas pessoas</h1>
+        {!showStories && (
+          <button onClick={() => setShowStories(true)}>
+            <img src={feed} alt="" />
+          </button>
+        )}
+        {showStories && (
+          <div className="story">
+            <StoriesProvider stories={makeStories().stories} />
+          </div>
+        )}
+      </div>
+      <h3 className="subtitle">
+        A opinião dos outros clientes que pediram na sua região!
+      </h3>
+      <div className="feed">
+        <div className="header">
+          <div className="profile-infos">
+            <img
+              className="header__image"
+              src="https://avatars.githubusercontent.com/u/34448877?s=460&u=512ec647b404aae6d6030f384bf726559f15a8e2&v=4"
+              alt=""
+            />
+            <span>
+              <strong>Daniel</strong> recomenda o restaurante
+              <strong> Habib's</strong>
+              <div>13 de Jun</div>
+            </span>
+          </div>
+        </div>
+        {/* <img
+          className="image"
+          alt=""
+          src="https://static-images.ifood.com.br/image/upload/t_high/pratos/cbcf001a-26fb-4e0f-8024-6dce16c569d6/202009301842_Mbgc_c.jpg"
+        /> */}
+        <div
+          style={{
+            backgroundImage:
+              'url(https://static-images.ifood.com.br/image/upload/t_high/pratos/cbcf001a-26fb-4e0f-8024-6dce16c569d6/202009301842_Mbgc_c.jpg)',
+          }}
+          className="image"
+        />
+        <p className="review">
+          Prato perfeito, amei muito, mas a entrega é demorada...
+        </p>
+        <div className="actions">
+          <div className="like-container">
+            <img className="like" src={like} alt="" /> <span>6</span>
+          </div>
+          <button>Ver prato</button>
+        </div>
+      </div>
+    </div>
   );
 };
 
